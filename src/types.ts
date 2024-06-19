5e41adaf19726ba873e063ee4433a34de3d54983
Franklin Shera <fshera96@gmail.com>
2024-06-19 20:47:02 +0300

    types

---
+++ src/types.ts
@@ -0,0 +1,44 @@
+export type TEvent = {
+  id: number;
+  name: string;
+  description: string;
+  startsOn: string;
+  endsOn: string;
+  city: string;
+  neighborhood: string;
+  address: string;
+  number: string;
+  country: string;
+  postalCode: string;
+  region: string;
+  placeName: string;
+  ticketUrl: string | null;
+  complement: string;
+  instagramUsername: string;
+  phoneNumber: string;
+  geoLocation: string;
+  media: {
+    id: number;
+    type: string;
+    fileName: string;
+    url: string;
+  }[];
+  categories: string[];
+  tags: string[];
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+  userId: number;
+  _esMeta: {
+    sort: [number, string];
+  };
+  score: number;
+};
