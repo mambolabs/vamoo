90fb7fe5596611c4c128e0c90e6c6128d6d0de0f
Franklin Shera <fshera96@gmail.com>
2024-08-05 15:07:51 +0300

    add constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,16 @@ export const EVENTS_ENDPOINT = "https://api.vamoo.la/v1/events";
  */
 export const AD_STEP = 5;
 
+/**
+ * Max number of tags in filter
+ */
+export const MAX_TAGS = 5;
+
+/**
+ * Max number of categories in filter
+ */
+export const MAX_CATEGORIES = 3;
+
 export const categories: string[] = [
   "outdoors",
   "beneficent",
