39cf0d5ae295fac96f903edcfcea0248c73cc2f4
Franklin Shera <fshera96@gmail.com>
2024-08-07 02:52:22 +0300

    add icons

--- src/components/common/Head.tsx
+++ src/components/common/Head.tsx
@@ -15,7 +15,18 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
